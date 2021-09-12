2f3c6d401e8b394595ee877833f7d430f663435b
Aadil Shabir <aadil.shabir13@gmail.com>
2021-09-12 10:20:20 +0500

    First commit for code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Calendar from './pages/Calendar';
+import Reports from './pages/Reports';
+import Settings from './pages/Settings';
+import Timeline from './pages/Timeline';
+import {Route, Switch} from 'react-router-dom'
+import Layout from './components/UI/Layout';
+import { makeStyles } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    marginTop: 50
+  }
+})
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+    <Layout />
+    <Switch>
+     <Route path='/timeline' exact>
+       <Timeline />
+     </Route>
+     <Route path='/reports' exact>
+       <Reports />
+     </Route>
+     <Route path='/calendar' exact>
+       <Calendar />
+     </Route>
+     <Route path='/settings' exact>
+       <Settings />
+     </Route>
+     <Route path='/login' exact>
+       <Login />
+     </Route>
+     <Route path='/signup' exact>
+       <Signup />
+     </Route>
+    </Switch>
     </div>
   );
 }
