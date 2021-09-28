fa65303e1fa0434f04ba7befb0ed81ce453a376f
Aadil Shabir <aadil.shabir13@gmail.com>
2021-09-28 13:48:56 +0500

    protect routes from external threats to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Login from './pages/login/index';
 import Signup from './pages/signup/index';
 import Calendar from './pages/Calendar';
@@ -7,9 +7,10 @@ import Settings from './pages/Settings';
 import Timeline from './pages/Timeline';
 import {createTheme} from '@material-ui/core/styles';
 import {ThemeProvider} from '@material-ui/styles';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import Layout from './components/UI/Layout/Layout';
 import {makeStyles} from '@material-ui/core';
+import AuthContext from './store/auth-context';
 
 const theme = createTheme({
   palette: {
@@ -17,11 +18,6 @@ const theme = createTheme({
       main: '#33ffad',
     },
   },
-  // typography: {
-  //   h2: {
-  //     fontFamily: 'Train One'
-  //   }
-  // }
 });
 
 const useStyles = makeStyles({
@@ -32,30 +28,53 @@ const useStyles = makeStyles({
 });
 
 const App: React.FC = () => {
+  const authCtx = useContext(AuthContext);
   const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
       <div className={classes.container}>
         <Layout />
         <Switch>
-          <Route path="/timeline" exact>
-            <Timeline />
-          </Route>
-          <Route path="/reports" exact>
-            <Reports />
-          </Route>
-          <Route path="/calendar" exact>
-            <Calendar />
-          </Route>
-          <Route path="/settings" exact>
-            <Settings />
-          </Route>
-          <Route path="/login" exact>
-            <Login />
-          </Route>
-          <Route path="/signup" exact>
-            <Signup />
-          </Route>
+          {authCtx.isLoggedIn && (
+            <Route path="/timeline" exact>
+              <Timeline />
+            </Route>
+          )}
+          {authCtx.isLoggedIn && (
+            <Route path="/reports" exact>
+              <Reports />
+            </Route>
+          )}
+          {authCtx.isLoggedIn && (
+            <Route path="/calendar" exact>
+              <Calendar />
+            </Route>
+          )}
+          {authCtx.isLoggedIn && (
+            <Route path="/settings" exact>
+              <Settings />
+            </Route>
+          )}
+          {!authCtx.isLoggedIn && (
+            <Route path="/login" exact>
+              <Login />
+            </Route>
+          )}
+          {!authCtx.isLoggedIn && (
+            <Route path="/signup" exact>
+              <Signup />
+            </Route>
+          )}
+          {authCtx.isLoggedIn && (
+            <Route path="*">
+              <Redirect to="/timeline"></Redirect>
+            </Route>
+          )}
+          {!authCtx.isLoggedIn && (
+            <Route path="*">
+              <Redirect to="/" />
+            </Route>
+          )}
         </Switch>
       </div>
     </ThemeProvider>
