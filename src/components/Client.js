3b49cfcb133a301edd99d65adb8525bb5bd71056
unknown <aadil.shabir13@gmail.com>
2022-01-19 13:35:31 +0500

    Restructured Page, Navbar, and Sidebar

--- src/components/Client.js
+++ src/components/Client.js
@@ -13,23 +13,25 @@ const Client = () => {
             <div class="col-2">
             <div class="vertical-nav bg-white" id="sidebar">
                 <ul class="nav flex-column bg-white mb-0">
-                <li class="nav-item"><Link to="/" class="nav-link text-dark font-italic bg-light "><i class="bi bi-list-ul"></i> &nbsp; &nbsp; &nbsp; All Clients</Link></li>
-                <li class="nav-item"><Link to="/AddClient" class="nav-link text-dark font-italic"><i class="bi bi-plus-square"></i> &nbsp; &nbsp; &nbsp; New Client</Link></li>
+                <li class="nav-item sidebar"><Link to="/" class="nav-link text-dark font-italic " style={{color: 'white'}}><i class="bi bi-list-ul"></i> &nbsp; &nbsp; All Clients</Link></li>
+                <li class="nav-item"><Link to="/AddClient" class="nav-link text-dark font-italic"><i class="bi bi-plus-square"></i> &nbsp; &nbsp; New Client</Link></li>
                 </ul>
             </div>
             </div>
 
-            <div class="col-10" >
-                <div  style={{background:"white",height:"7rem",width:"85rem",display:"flex",flexDirection:"row"}}>    
-                    <p style={{fontSize: "25px" , fontWeight:"bold",marginLeft:"2%" ,marginTop:"1rem"}}> Clients</p>
+            <div class="col-10" style={{padding: '0rem'}} >
+                
+                <div style={{background:"white",height:"7rem",width:"82vw",display:"flex",flexDirection:"row"}}>    
+                    <p style={{fontSize: "25px" , fontWeight:"bold",marginLeft:"2%" ,marginTop:"2rem"}}> Clients</p>
                     <div style={{fontSize: "25px" ,marginTop:"1rem", fontWeight:"bold",float:"right" ,marginLeft:"75%" }}>  
                     <Link  className="btn btn-default" to="/AddClient"> <i class="bi bi-plus-square"></i>&nbsp;Add new client</Link></div>
                 </div>
-                <div  style={{background:"white",marginTop:"2%",marginLeft:"1%" ,height:"40rem",width:"84rem",display:"flex",flexDirection:"row"}}>
+                <div  style={{background:"white",marginTop:"2%",marginLeft:"3%" ,height:"25rem",width:"79.5vw",display:"flex",flexDirection:"row"}}>
                     <div style={{marginLeft:"7rem" ,marginTop:"2%"}}> 
                         <Clientsdata></Clientsdata>
                     </div>
                 </div>
+                
             </div>
         </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
 import { NavLink } from 'react-router-dom';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
   
 const Header = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-custom">
-                <div class="navbar-nav">
+            <Navbar collapseOnSelect expand="sm" className="navbar-custom"  style={{paddingRight: '100vw'}}>
+                <Container>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+            {/* <nav class="navbar navbar-expand-lg navbar-custom">
+                <div class="navbar-nav"> */}
                     
-                    <NavLink className="navbar-brand nav-item  nav-link" activeClassName="selected" to="/" exact={true}> Clients</NavLink>
-                    <NavLink className="navbar-brand nav-item  nav-link " activeClassName="selected" to="/operator" exact={true}>  Operators </NavLink>
-                    <NavLink className="navbar-brand nav-item  nav-link" activeClassName="selected" to="/campaign" exact={true}>Compaigns</NavLink>
-                    <NavLink className="navbar-brand nav-item  nav-link" activeClassName="selected" to="/Subscription"exact={true}>Subscription</NavLink>
-                    <NavLink className="navbar-brand nav-item nav-link" activeClassName="selected" to="/Transaction"exact={true}>Transaction</NavLink>
-                </div>
-            </nav>
+                    <NavLink className="navbar-brand nav-item nav-link" style={{color: 'white'}} activeClassName="selected" to="/" exact={true}> Clients</NavLink>
+                    <NavLink className="navbar-brand nav-item nav-link" style={{color: 'white'}} activeClassName="selected" to="/operator" exact={true}>  Operators </NavLink>
+                    <NavLink className="navbar-brand nav-item nav-link" style={{color: 'white'}} activeClassName="selected" to="/campaign" exact={true}>Compaigns</NavLink>
+                    <NavLink className="navbar-brand nav-item nav-link" style={{color: 'white'}} activeClassName="selected" to="/Subscription" exact={true}>Subscription</NavLink>
+                    <NavLink className="navbar-brand nav-item nav-link" style={{color: 'white'}} activeClassName="selected" to="/Transaction" exact={true}>Transaction</NavLink>
+                {/* </div>
+            </nav> */}
+            </Nav>
+            </Navbar.Collapse>
+            </Container>
+            </Navbar>
         
         </div>
     )
